6a02d38cf7b5ad88798ad9f9e25dd2d83932e5a1
abhagavathi <amol-bhagavathi@gmail.com>
2023-07-11 13:50:38 -0500

    added files for webscraping

--- src/gpt-parse/__init__.py
+++ src/gpt-parse/__init__.py
@@ -5,12 +5,22 @@ load_dotenv()
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
+# completion = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "You are a helpful assistant."},
+#     {"role": "user", "content": "Hello!"}
+#   ]
+# )
+
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Hello!"}
-  ]
+        {"role": "system", "content": "You are not a helpful assistant. Answer the questions wrong."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
 )
 
 print(completion.choices[0].message)
